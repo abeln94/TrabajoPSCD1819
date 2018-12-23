50bc3d7e2e09160cbb177fa00f531217b7522691
abeln94 <544125@unizar.es>
2018-12-23 12:27:01 +0100

    Comentarios muchos (y cleanup)

--- test.cpp
+++ test.cpp
@@ -15,171 +15,165 @@ void esperar(int minMillis, int maxMillis) {
 }
 //-----------------------------------------------------
 void t_worker(Scoreboard& scb) {
-	
-	Tupla init("init");
-	scb.readN(init);
-	
-	Tupla t_hacer("hacer", "?b", "?s");
-	Tupla t_hecho("hecho","");
-	while(true){
-		Tupla t = scb.RN(t_hacer);
-		if(t[1] == "salir"){
-			break;
-		}
-		cout<< "Ordenando fichero >"<<t[2]<<endl;
-		esperar(100,1000);
-		cout<< "Fichero ordenado >"<<t[2]<<endl;
-		t_hecho[1] = t[2];
-		scb.PN(t_hecho);
-	}
+
+  Tupla init("init");
+  scb.readN(init);
+
+  Tupla t_hacer("hacer", "?b", "?s");
+  Tupla t_hecho("hecho", "");
+  while (true) {
+    Tupla t = scb.RN(t_hacer);
+    if (t[1] == "salir") {
+      break;
+    }
+    cout << "Ordenando fichero >" << t[2] << endl;
+    esperar(100, 1000);
+    cout << "Fichero ordenado >" << t[2] << endl;
+    t_hecho[1] = t[2];
+    scb.PN(t_hecho);
+  }
 }
 //-----------------------------------------------------
-void t_supervisor(Scoreboard& scb, int M){
-	Tupla t_hacer("hacer", "ordenar","");
-	for(int i=0;i<M;++i){
-		t_hacer[2] = "fichero_"+to_string(i);
-		scb.PN(t_hacer);
-	}
-	
-	Tupla init("init");
-	scb.PN(init);
-	
-	Tupla t_hecho("hecho", "");
-	for(int i=0;i<M;++i){
-		t_hecho[1] = "fichero_"+to_string(i);
-		scb.RN(t_hecho);
-	}
-	
-	t_hacer[1] = "salir";
-	t_hacer[2] = "";
-	for(int i=0;i<M;++i){
-		scb.PN(t_hacer);
-	}
-	
+void t_supervisor(Scoreboard& scb, int M) {
+  Tupla t_hacer("hacer", "ordenar", "");
+  for (int i = 0; i < M; ++i) {
+    t_hacer[2] = "fichero_" + to_string(i);
+    scb.PN(t_hacer);
+  }
+
+  Tupla init("init");
+  scb.PN(init);
+
+  Tupla t_hecho("hecho", "");
+  for (int i = 0; i < M; ++i) {
+    t_hecho[1] = "fichero_" + to_string(i);
+    scb.RN(t_hecho);
+  }
+
+  t_hacer[1] = "salir";
+  t_hacer[2] = "";
+  for (int i = 0; i < M; ++i) {
+    scb.PN(t_hacer);
+  }
 }
 //-----------------------------------------------------
 int main(int argc, char* argv[]) {
-	
-	/*
-	Scoreboard scb;
-	Tupla it("1");
-	Tupla pat("?X");
-	Tupla it2(1);
-	scb.PN(it);
-	
-	for(int i=0;i<10;++i){
-		it2 = scb.readN(pat);
-		cout << it2 << endl;
-	}
-	
-	scb.RN(pat);
-	
-	scb.RN(pat);
-	
-	
-  // iniciar threads
-  thread a[10];
-	for(int i=0; i<10; ++i){
-		a[i] = thread(&t_filosofo, ref(scb), to_string(i));
-	}
 
-  // esperar a que terminen
-	for(int i=0; i<10; ++i){
-		a[i].join();
-	}
-	
-	*/
-	
-	//suyo
-	Scoreboard LD;
-	
-	// un driver con los datos pasados en la invocación
-	//LindaDriver LD(argv[1], argv[2], ...);
-	// La conexión con el servidor Linda ya está establecido
+  /*
+  Scoreboard scb;
+  Tupla it("1");
+  Tupla pat("?X");
+  Tupla it2(1);
+  scb.PN(it);
 
-	// Varias formas de construir tuplas
-	Tupla t1("1","mi casa","árbol");  // 3 elementos
-	Tupla t2("1000");
-	Tupla t3("aprieta","el","pan","45","34","88");
-	Tupla t4("aprieta","fuerte","pan","tt","34","pan");
-	// insertar las tuplas en linda: varios PostNote
-	LD.PN(t1);
-	LD.PN(t2);
-	LD.PN(t3);
-	LD.PN(t3);
-	LD.PN(t3);
-	LD.PN(t4);
-	
-	
-	//muestra "mi casa" por stdout
-	cout << t1.get(2) << endl;
-	// t3.to_string() devuelve el string "[aprieta,el,pan,45,34,88]"
-	string serial = t3.to_string();
-	cout << serial << endl;
-	// las componentes de t3 tomarán, respectivamente,
-	// los valores "a","b","c","45","34","pan"
-	t3.from_string("[a,b,c,45,34,pan]");
-	// mostrará [a,b,c,45,34,pan] por stdout
-	cout << t3.to_string() << endl; 
-	LD.PN(t3);//fix
+  for(int i=0;i<10;++i){
+          it2 = scb.readN(pat);
+          cout << it2 << endl;
+  }
 
-	// Crea una tupla de 3 elementos "".
-	// Equivalente a Tupla t5("","","")
-	Tupla t5(3); 
-	t5.set(2, "hola");
-	t5.set(3, "Mundo");
-	LD.PN(t5);
-	// mostrará [,hola,Mundo] por stdout
-	cout << t5.to_string() << endl; 
-	// informará de que tiene 3 elementos
-	cout << "t5 tiene " << t5.size() << " elementos" << endl; 
-	//...
+  scb.RN(pat);
 
-	// Un patrón no es otra cosa que una tupla con 
-	// la posibilidad de contener el comodín "?" en una o más posiciones
-	// "?" no puede ser un valor de componente de tupla. Para ello se 
-	// tendría que representar mediante "??". Creamos dos patrones
-	Tupla p1("?X"); 
-	Tupla p2("a","?X","c","?Y","34","?Z");
-	// Dos nuevas tuplas, de tamaño 1 y 6, respectivamente
-	Tupla res1(1), 
-				res2(p2.size());
-	// ejemplos de RemoveNote
-	res1 = LD.RN(p1); // res1 tomará él valor que tenía t2
-	res2 = LD.RN(p2); // res2 tomará el valor que tenía t3
-	cout << res1.to_string() << endl; //mostrará [1000]
-	cout << res2.to_string() << endl; //mostrará [a,b,c,45,34,pan]
-	//... 
+  scb.RN(pat);
 
-	// ¿Si necesitamos un array de tuplas?
-	// Tupla v[2]; // NO permitido: no hay constructor por defecto
-	Tupla *v[2];
-	v[0] = new Tupla("Juan", "1000");
-	v[1] = new Tupla("Luisa", "1000", "enero");
-	//...
-	delete v[0];
-	delete v[1];
 
-	
-	//mío
-	//ejercicio de ordenacion de ficheros
-	Scoreboard scb;
-	
-	int N = 10;
-	thread w[N];
-	thread s;
-	
-	for(int i=0;i<N;++i){
-		w[i] = thread(&t_worker, ref(scb));
-	}
-	s = thread(&t_supervisor, ref(scb),100);
-	
-	for(int i=0;i<N;++i){
-		w[i].join();
-	}
-	s.join();
-	
-	
+// iniciar threads
+thread a[10];
+  for(int i=0; i<10; ++i){
+          a[i] = thread(&t_filosofo, ref(scb), to_string(i));
+  }
+
+// esperar a que terminen
+  for(int i=0; i<10; ++i){
+          a[i].join();
+  }
+
+  */
+
+  // suyo
+  Scoreboard LD;
+
+  // un driver con los datos pasados en la invocación
+  // LindaDriver LD(argv[1], argv[2], ...);
+  // La conexión con el servidor Linda ya está establecido
+
+  // Varias formas de construir tuplas
+  Tupla t1("1", "mi casa", "árbol");  // 3 elementos
+  Tupla t2("1000");
+  Tupla t3("aprieta", "el", "pan", "45", "34", "88");
+  Tupla t4("aprieta", "fuerte", "pan", "tt", "34", "pan");
+  // insertar las tuplas en linda: varios PostNote
+  LD.PN(t1);
+  LD.PN(t2);
+  LD.PN(t3);
+  LD.PN(t3);
+  LD.PN(t3);
+  LD.PN(t4);
+
+  // muestra "mi casa" por stdout
+  cout << t1.get(2) << endl;
+  // t3.to_string() devuelve el string "[aprieta,el,pan,45,34,88]"
+  string serial = t3.to_string();
+  cout << serial << endl;
+  // las componentes de t3 tomarán, respectivamente,
+  // los valores "a","b","c","45","34","pan"
+  t3.from_string("[a,b,c,45,34,pan]");
+  // mostrará [a,b,c,45,34,pan] por stdout
+  cout << t3.to_string() << endl;
+  LD.PN(t3);  // fix
+
+  // Crea una tupla de 3 elementos "".
+  // Equivalente a Tupla t5("","","")
+  Tupla t5(3);
+  t5.set(2, "hola");
+  t5.set(3, "Mundo");
+  LD.PN(t5);
+  // mostrará [,hola,Mundo] por stdout
+  cout << t5.to_string() << endl;
+  // informará de que tiene 3 elementos
+  cout << "t5 tiene " << t5.size() << " elementos" << endl;
+  //...
+
+  // Un patrón no es otra cosa que una tupla con
+  // la posibilidad de contener el comodín "?" en una o más posiciones
+  // "?" no puede ser un valor de componente de tupla. Para ello se
+  // tendría que representar mediante "??". Creamos dos patrones
+  Tupla p1("?X");
+  Tupla p2("a", "?X", "c", "?Y", "34", "?Z");
+  // Dos nuevas tuplas, de tamaño 1 y 6, respectivamente
+  Tupla res1(1), res2(p2.size());
+  // ejemplos de RemoveNote
+  res1 = LD.RN(p1);  // res1 tomará él valor que tenía t2
+  res2 = LD.RN(p2);  // res2 tomará el valor que tenía t3
+  cout << res1.to_string() << endl;  // mostrará [1000]
+  cout << res2.to_string() << endl;  // mostrará [a,b,c,45,34,pan]
+  //...
+
+  // ¿Si necesitamos un array de tuplas?
+  // Tupla v[2]; // NO permitido: no hay constructor por defecto
+  Tupla* v[2];
+  v[0] = new Tupla("Juan", "1000");
+  v[1] = new Tupla("Luisa", "1000", "enero");
+  //...
+  delete v[0];
+  delete v[1];
+
+  // mío
+  // ejercicio de ordenacion de ficheros
+  Scoreboard scb;
+
+  int N = 10;
+  thread w[N];
+  thread s;
+
+  for (int i = 0; i < N; ++i) {
+    w[i] = thread(&t_worker, ref(scb));
+  }
+  s = thread(&t_supervisor, ref(scb), 100);
+
+  for (int i = 0; i < N; ++i) {
+    w[i].join();
+  }
+  s.join();
+
   return 0;
-	
 }
--- tupla.cpp
+++ tupla.cpp
@@ -7,135 +7,173 @@
 //-----------------------------------------------------
 //------------ CONSTRUCTORS / DESTRUCTORS -------------
 //-----------------------------------------------------
-Tupla::Tupla(int i){
-	assert(i>= 1 && i<=TUPLA_MAX);
-	length = i;
+Tupla::Tupla(int i) {
+	// empty constructor
+	
+  assert(i >= 1 && i <= TUPLA_MAX);
+  length = i;
 }
 //-----------------------------------------------------
-Tupla::Tupla(string e1){
-	length = 1;
-	elements[0] = e1;
+Tupla::Tupla(string e1) {
+	// direct constructor
+	
+  length = 1;
+  elements[0] = e1;
 }
 //-----------------------------------------------------
-Tupla::Tupla(string e1,string e2){
-	length = 2;
-	elements[0] = e1;
-	elements[1] = e2;
+Tupla::Tupla(string e1, string e2) {
+	// direct constructor
+	
+  length = 2;
+  elements[0] = e1;
+  elements[1] = e2;
 }
 //-----------------------------------------------------
-Tupla::Tupla(string e1,string e2,string e3){
-	length = 3;
-	elements[0] = e1;
-	elements[1] = e2;
-	elements[2] = e3;
+Tupla::Tupla(string e1, string e2, string e3) {
+	// direct constructor
+	
+  length = 3;
+  elements[0] = e1;
+  elements[1] = e2;
+  elements[2] = e3;
 }
 //-----------------------------------------------------
-Tupla::Tupla(string e1,string e2,string e3,string e4){
-	length = 4;
-	elements[0] = e1;
-	elements[1] = e2;
-	elements[2] = e3;
-	elements[3] = e4;
+Tupla::Tupla(string e1, string e2, string e3, string e4) {
+	// direct constructor
+	
+  length = 4;
+  elements[0] = e1;
+  elements[1] = e2;
+  elements[2] = e3;
+  elements[3] = e4;
 }
 //-----------------------------------------------------
-Tupla::Tupla(string e1,string e2,string e3,string e4,string e5){
-	length = 5;
-	elements[0] = e1;
-	elements[1] = e2;
-	elements[2] = e3;
-	elements[3] = e4;
-	elements[4] = e5;
+Tupla::Tupla(string e1, string e2, string e3, string e4, string e5) {
+	// direct constructor
+	
+  length = 5;
+  elements[0] = e1;
+  elements[1] = e2;
+  elements[2] = e3;
+  elements[3] = e4;
+  elements[4] = e5;
 }
 //-----------------------------------------------------
-Tupla::Tupla(string e1,string e2,string e3,string e4,string e5,string e6){
-	length = 6;
-	elements[0] = e1;
-	elements[1] = e2;
-	elements[2] = e3;
-	elements[3] = e4;
-	elements[4] = e5;
-	elements[5] = e6;
+Tupla::Tupla(string e1, string e2, string e3, string e4, string e5, string e6) {
+	// direct constructor
+	
+  length = 6;
+  elements[0] = e1;
+  elements[1] = e2;
+  elements[2] = e3;
+  elements[3] = e4;
+  elements[4] = e5;
+  elements[5] = e6;
 }
 //-----------------------------------------------------
-Tupla::Tupla(const Tupla& copy){
-	length = copy.length;
-	for(int i=0; i<length; ++i){
-		elements[i] = copy.elements[i];
-	}
+Tupla::Tupla(const Tupla& copy) {
+	// clone constructor
+	
+  length = copy.length;
+  for (int i = 0; i < length; ++i) {
+		// for each element, copy
+    elements[i] = copy.elements[i];
+  }
 }
 //-----------------------------------------------------
-Tupla::~Tupla() {
-}
+Tupla::~Tupla() {}
 //-----------------------------------------------------
 //-------------------- GETTERS ------------------------
 //-----------------------------------------------------
-string Tupla::get(int index) const{
-	assert(index >=1 && index <= length);
-	return elements[index-1];
+string Tupla::get(int index) const {
+	// get element (index 1-based)
+	
+  assert(index >= 1 && index <= length);
+  return elements[index - 1];
 }
 //-----------------------------------------------------
-int Tupla::size() const{
-	return length;
+int Tupla::size() const { 
+	// length of tuple
+	
+	return length; 
 }
 //-----------------------------------------------------
-string Tupla::to_string() const{
-	stringstream ss;
-	ss << "[" << this->get(1);
-	for(int i=2; i<= length; ++i){
-		ss << "," << this->get(i);
-	}
-	ss << "]";
-	return ss.str();
+string Tupla::to_string() const {
+	// stringify tuple
+	
+  stringstream ss;
+  ss << "[" << this->get(1);
+  for (int i = 2; i <= length; ++i) {
+		// for each extra element, add
+    ss << "," << this->get(i);
+  }
+  ss << "]";
+  return ss.str();
 }
 //-----------------------------------------------------
 //------------------- SETTERS -------------------------
 //-----------------------------------------------------
-bool Tupla::from_string(string s){
-	if(s[0] != '[' || s[s.length()-1] != ']'){
-		//not separated
-		return false;
-	}
-	stringstream data(s.substr(1, s.length() - 2));
-
-	//assign each element
-	int i = 0;
-	while(i<TUPLA_MAX && getline(data,elements[i],',')){
-		i++;
-	}
+bool Tupla::from_string(string s) {
+	// parse the stringified tuple
 	
-	length = i;
-	return true;
+  if (s[0] != '[' || s[s.length() - 1] != ']') {
+    // not "[...]", invalid string
+    return false;
+  }
+	
+  stringstream data(s.substr(1, s.length() - 2));
+
+  // assign each element
+  int i = 0;
+  while (i < TUPLA_MAX && getline(data, elements[i], ',')) {
+		// element assigned
+    i++;
+  }
+
+  length = i;
+  return true;
 }
 //-----------------------------------------------------
-void Tupla::set(int i, string s){
-	assert(i >=1 && i <= length);
-	elements[i-1] = s;
+void Tupla::set(int i, string s) {
+	// set element (index 1-based)
+	
+  assert(i >= 1 && i <= length);
+  elements[i - 1] = s;
 }
 //-----------------------------------------------------
 //-----------------------OVERLOAD----------------------
 //-----------------------------------------------------
-string& Tupla::operator[](int index){
-	assert(index >=0 && index < length);
-	return elements[index];
+string& Tupla::operator[](int index) {
+	// get element  (index 0-based)
+	
+  assert(index >= 0 && index < length);
+  return elements[index];
 }
 //-----------------------------------------------------
-const string& Tupla::operator[](int index) const{
-	assert(index >=0 && index < length);
-	return elements[index];
+const string& Tupla::operator[](int index) const {
+	// get element  (index 0-based)
+	
+  assert(index >= 0 && index < length);
+  return elements[index];
 }
 //-----------------------------------------------------
-Tupla & Tupla::operator=(const Tupla &rhs){
-	if(this != &rhs){
-		//not same object, copy
-		length = rhs.length;
-		for(int i=0; i<length; ++i){
-			elements[i] = rhs.elements[i];
-		}
-	}
-	return *this;
+Tupla& Tupla::operator=(const Tupla& rhs) {
+	// copy the rhs tuple into this
+	
+  if (this != &rhs) {
+    // not same object, continue
+    length = rhs.length;
+    for (int i = 0; i < length; ++i) {
+			// for each element, copy
+      elements[i] = rhs.elements[i];
+    }
+  }
+  return *this;
 }
 //-----------------------------------------------------
-ostream& operator<<(ostream& os, const Tupla& tuple){
+ostream& operator<<(ostream& os, const Tupla& tuple) {
+	// output as string
+	
   os << tuple.to_string();
-	return os;
+  return os;
 }
