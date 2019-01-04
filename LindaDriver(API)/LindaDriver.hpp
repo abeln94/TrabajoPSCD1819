7f48cbf97b37f0e3634ff7ce0c56284dd99b8761
Uncastellum <danicorreo99@gmail.com>
2019-01-04 11:52:23 +0100

    edits:   misc

--- LindaDriver(API)/LindaDriver.hpp
+++ LindaDriver(API)/LindaDriver.hpp
@@ -10,8 +10,8 @@
 #ifndef LINDA_DRIVER
 #define LINDA_DRIVER
 
-//#include <iostream>
-//#include <Socket.hpp>
+#include <iostream>
+#include <Socket.hpp>
 //#include <string>
 //#include <mutex>
 //#include <condition_variable>
@@ -19,5 +19,22 @@
 //using namespace std;
 
 //...
+
+struct LindaDriver{
+  public:
+    LindaDriver(string ip, int puerto);
+    ~LindaDriver();
+
+    //...
+
+  private:
+    string ip_serv, ip_s1, ip_s2, ip_s3;
+    int port_serv, port_s1, port_s2, port_s3;
+    Socket *soc_serv;
+    Socket *soc_s1, *soc_s2, *soc_s3;
+
+
+
+}
 
 #endif
