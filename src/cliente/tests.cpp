478e11284d44dc7c743b426f3f618782f9f86e7b
abeln94 <544125@unizar.es>
2019-01-16 20:26:14 +0100

    Añadido ejemplo profesores, arreglado

--- src/cliente/tests.cpp
+++ src/cliente/tests.cpp
@@ -670,3 +670,90 @@ void commands(char* ip, int port, int _, char* command) {
 ADDFUNCTION(commands,
             "Envía un comando al servidor. ejecutar como '1 commands "
             "<COMMAND>'. Comandos admitidos: END, CLEAR, SIZE");
+
+//---------------------------------------------
+
+//-----------------------------------------------------
+void errorInvocacion() {
+    cerr << "Invocar como:" << endl;
+    cerr << "   clienteCarga <NT> <IP_LS> <Port_LS> ..." << endl;
+    cerr << "      <NT>: num de tuplas" << endl;
+    cerr << "      <IP_LS>: IP del servidor Linda" << endl;
+    cerr << "      <Port_LS>: puerto del servidor Linda" << endl;
+}
+//-----------------------------------------------------
+//int main(int argc, char* argv[]) {
+void clienteCarga(char* ip, int port, int _, char* command) {
+    const int MAX_LENGTH = 6; //máx longitud tuplas
+    //faltan argumentos en la invocación?
+    //if (argc < 4 ) {
+    //    errorInvocacion();
+    //    return 1;
+    //}
+    int NT = atoi(/*argv[1]*/command); //num de tuplas a tratar
+    // un driver con los datos pasados en la invocación
+    LindaDriver LD(/*argv[2], argv[3], ...*/ip,port);
+    // La conexión con el servidor Linda ya está establecido
+    //-----------------------------------------------------
+    //MAX_LENGTH tuplas para ir rellenando y enviando a Linda
+    //Se crean con cadenas vacías
+    Tupla *tuplas[MAX_LENGTH];
+    for (int j=0; j<MAX_LENGTH; j++) {
+        tuplas[j] = new Tupla(j+1);
+    }
+    cout << "Tuplas creadas" << endl;
+    //-----------------------------------------------------
+    Tupla *patrones[MAX_LENGTH]; //van a ser constantes
+    patrones[0] = new Tupla("?A");
+    patrones[1] = new Tupla("?A","?B");
+    patrones[2] = new Tupla("?A","?B","?C");
+    patrones[3] = new Tupla("?A","?B","?C","?D");
+    patrones[4] = new Tupla("?A","?B","?C","?D","?E");
+    patrones[5] = new Tupla("?A","?B","?C","?D","?E","?F");
+    cout << "Patrones creados" << endl;
+    //-----------------------------------------------------
+    // Insertar 6*NT tuplas
+    for (int i=1; i<=NT; i++) {
+        string vT[MAX_LENGTH];
+        vT[0] = to_string(i);
+        for (int j=1; j<MAX_LENGTH; j++) {
+            vT[j] = vT[j-1] + "," + to_string(i+j);
+        }
+
+        for (int j=0; j<MAX_LENGTH; j++) {
+            tuplas[j]->from_string('[' + vT[j] + ']');//fixed
+            // PosNote
+            LD.PN(*tuplas[j]);//fixed
+        }
+    }
+    cout << "Tuplas insertadas" << endl;
+    // Retirar 6*NT tuplas
+    for (int i=1; i<=NT; i++) {
+        for (int j=0; j<MAX_LENGTH; j++) {
+            // RemoveNote
+            *tuplas[j] = LD.RN(*patrones[j]); //misma longitud //fixed
+        }
+        const int GRUPO = 50;
+        // mostrar primera y última tuplas de cada grupo de GRUPO tuplas rescatadas
+        if ((i-1)%GRUPO == 0) {
+            cout << "    " << tuplas[0]->to_string() << endl;//fixed
+            cout << "    " << tuplas[MAX_LENGTH-1]->to_string() << endl;//fixed
+        }
+    }
+    cout << "Tuplas retiradas" << endl;
+    //-----------------------------------------------------
+    /*
+		delete[] tuplas;
+    delete[] patrones;
+		*/
+		//BIG fixed
+		for (int j=0; j<MAX_LENGTH; j++) {
+        delete tuplas[j];
+        delete patrones[j];
+    }
+    //-----------------------------------------------------
+
+    return; //return 0;
+}
+
+ADDFUNCTION( clienteCarga, "cliente de ejemplo profesores. Ejecutar como '1 clienteCarga N' siendo N el número de tuplas");
